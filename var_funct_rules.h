a886e04ab53260367cc9c42650b7bbfa550eac4f
ArnauldDyor <44143401+ArnauldDyor@users.noreply.github.com>
2018-12-05 19:41:13 +0100

    Add files via upload

---
+++ var_funct_rules.h
@@ -0,0 +1,17 @@
+/*
+
+** FILENAME : Linter
+
+** MADE BY : Ilès Benkoussa, Arnauld Biam, Victor Meyer
+
+** Prototype of "var_funct_rules" function
+
+*/
+
+void unsed_variable(char *tabChar, char *nomFich);
+
+void unsed_function(char *tabChar, char *nomFich);
+
+void undeclared_variable(char *tabChar, char *nomFich);
+
+void undeclared_function(char *tabChar, char *nomFich);
